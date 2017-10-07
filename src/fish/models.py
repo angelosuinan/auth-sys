c5dfc25737f96669d288c4e0881b4bad74f9f22f
angelo suinan <suinanangelo@gmail.com>
2017-10-07 13:45:18 +0800

    add category to Fish model

--- src/fish/models.py
+++ src/fish/models.py
@@ -4,10 +4,18 @@ from django.db import models
 from core.models import Base
 
 
+class Category(Base):
+    name = models.CharField(max_length=50, blank=False)
+
+    def __str__(self,):
+        return self.name
+
+
 class Fish(Base):
-    name = models.CharField(max_length=30, blank=False)
+    name = models.CharField(max_length=50, blank=False)
     scientific_name = models.CharField(max_length=75, blank=True)
     image = models.FileField(upload_to='fish/', blank=True, null=True)
+    category = models.ForeignKey(Category)
 
     def __str__(self):
         return self.name
